cad24b930bf3b4292104342f21b6c68b37fefb58
SGS Sandhu <sgs.sandhu@gmail.com>
2018-01-08 18:13:30 +0530

    Update 20180108124330

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,71 @@ function init_columns(){
     });
 }
 
+/*****************************************************
+/*  initialize_sections
+/*****************************************************/
+function initializaSection(){
 
+	var aione_section = new Array();
+	$(".aione-builder section").each(function() {
+		var aione_sec_id = $(this).attr("id");
+		if(aione_sec_id != undefined){
+			aione_section.push(aione_sec_id);
+		} else {
+			var aione_sec_id = 'section_'+Math.floor(Math.random()*100000000);
+			$(this).attr("id", aione_sec_id);
+			aione_section.push(aione_sec_id);
+		}
+        
+    });
+   
+    $.each(aione_section, function( index, aione_sec ) {
+    	var aione_sec_id = document.getElementById(aione_sec);
+    	
+    	Sortable.create(aione_sec_id,{
+			//group: "name_"+(index+1),  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
+			group: { name: "rows", pull: true, put: ['aione_elements_row'] },
+			sort: true,  // sorting inside list
+			delay: 0, // time in milliseconds to define when the sorting should start
+			disabled: false, // Disables the sortable if set to true.
+			store: null,  // @see Store
+			handle: '.aione-col-handle',
+			animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
+		});
+    });
+}
+
+/*****************************************************
+/*  Initialize ROWS
+/*****************************************************/
+function InitializeRow(){
+	var aione_rows = new Array();
+	$(".aione-builder .ar").each(function() {
+		var aione_row_id = $(this).attr("id");
+		if(aione_row_id != undefined){
+			aione_rows.push(aione_row_id);
+		} else {
+			var aione_row_id = 'el_id_'+Math.floor(Math.random()*100000000);
+			$(this).attr("id", aione_row_id);
+			aione_rows.push(aione_row_id);
+		}
+        
+    });
+    $.each(aione_rows, function( index, aione_row ) {
+    	var aione_row_id = document.getElementById(aione_row);
+    	
+    	Sortable.create(aione_row_id,{
+			//group: "name_"+(index+1),  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
+			group: { name: "rows", pull: true, put: ['aione_elements_columns','aione_elements'] },
+			sort: true,  // sorting inside list
+			delay: 0, // time in milliseconds to define when the sorting should start
+			disabled: false, // Disables the sortable if set to true.
+			store: null,  // @see Store
+			handle: '.aione-col-handle',
+			animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
+		});
+    });
+}
 $(document).ready(function() {
 	init_columns();
 	init_rows();
@@ -117,6 +181,50 @@ $(document).ready(function() {
 		$(".aione-builder-wrapper").toggleClass("mode-preview");
 		$(".aione-builder-wrapper").toggleClass("mode-editable");
 	});
+
+	var aione_elements_section = document.getElementById('aione_elements_section');
+	Sortable.create(aione_elements_section,{
+		group: { name: "aione_elements_section", pull: 'clone', put: true },
+		sort: false,
+		onEnd: function(){
+				setTimeout(function(){
+					initializaSection();
+				},200);
+			}
+	});
+	initializaSection();
+
+	var aione_builder = document.getElementById('aione_section');
+	Sortable.create(aione_builder,{
+		//group: "name_"+(index+1),  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
+		group: { name: "aione_builder_sections", pull: false, put: ['aione_elements_row'] },
+		sort: true,  // sorting inside list
+		delay: 0, // time in milliseconds to define when the sorting should start
+		disabled: false, // Disables the sortable if set to true.
+		store: null,  // @see Store
+		animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
+		// handle: ".aione-section-handle",
+		ghostClass: "ghost-section", 
+		// chosenClass: "chosen-section",
+		dragClass:"drag-section",
+		//filter: '.ar , .ac'
+	});
+	
+
+	var aione_elements_row = document.getElementById('aione_elements_row');
+	Sortable.create(aione_elements_row,{
+		group: { name: "aione_elements_row", pull: 'clone', put: ['aione_elements_columns'] },
+		sort: false,
+		onEnd: function(){
+					setTimeout(function(){
+						InitializeRow();
+						
+					},200);
+					
+				}
+	});
+	InitializeRow();
+
 });
 
 		 
