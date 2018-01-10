fefd7b36c2098e6a67451ffc7179398944b2ba08
SGS Sandhu <sgs.sandhu@gmail.com>
2018-01-10 21:19:59 +0530

    Update 20180110154959

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,20 +16,47 @@ function get_page_content() {
 /*  init_sections
 /*****************************************************/
 function init_sections(){
-	var aione_builder = document.getElementById('aione_builder');
-	Sortable.create(aione_builder,{
-		group: "aione_sections",
-		sort: true,
-		delay: 0,
-		disabled: false,
-		store: null,
-		//filter: '.actions',
-		handle: '.section-handle',
-		ghostClass: 'ghost-section',
-		chosenClass: 'chosen-section',
-		dragClass: 'drag-section', 
-		animation: 150
-	});
+	// var aione_builder = document.getElementById('aione_builder');
+	// Sortable.create(aione_builder,{
+	// 	group: "aione_sections",
+	// 	sort: true,
+	// 	delay: 0,
+	// 	disabled: false,
+	// 	store: null,
+	// 	//filter: '.actions',
+	// 	handle: '.section-handle',
+	// 	ghostClass: 'ghost-section',
+	// 	chosenClass: 'chosen-section',
+	// 	dragClass: 'drag-section', 
+	// 	animation: 150
+	// });
+	var aione_sections = new Array();
+	$(".aione-builder").each(function() {
+		var aione_sec_id = $(this).attr("id");
+		if(aione_sec_id != undefined){
+			aione_sections.push(aione_sec_id);
+		} else {
+			var aione_sec_id = 'build_id_'+Math.floor(Math.random()*100000000);
+			$(this).attr("id", aione_sec_id);
+			aione_sections.push(aione_sec_id);
+		}
+    });
+    console.log("= = =");
+    console.log(aione_sections);
+    $.each(aione_sections, function( index, aione_sec ) {
+    	var aione_sec_id = document.getElementById(aione_sec);
+    	
+    	Sortable.create(aione_sec_id,{
+			//group: "name_"+(index+1),  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
+			group: { name: "rows", pull: true, put: ['aione_elements','aione_elements_section'] },
+			sort: true,  // sorting inside list
+			delay: 0, // time in milliseconds to define when the sorting should start
+			disabled: false, // Disables the sortable if set to true.
+			store: null,  // @see Store
+			handle: '.aione-col-handle',
+			animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
+		});
+    });
 }
 
 /*****************************************************
@@ -67,7 +94,7 @@ function init_rows(){
 /*****************************************************/
 function init_columns(){
 	var aione_rows = new Array();
-	$(".aione-builder > section > .ar").each(function() {
+	$(".aione-builder > section  .ar").each(function() {
 		var aione_row_id = $(this).attr("id");
 		if(aione_row_id != undefined){
 			aione_rows.push(aione_row_id);
@@ -95,9 +122,9 @@ function init_columns(){
 }
 
 /*****************************************************
-/*  initialize_sections
+/*  function triggers on drop of a section
 /*****************************************************/
-function initializaSection(){
+function onDropSection(){
 
 	var aione_section = new Array();
 	$(".aione-builder section").each(function() {
@@ -129,9 +156,9 @@ function initializaSection(){
 }
 
 /*****************************************************
-/*  Initialize ROWS
+/*   function triggers on drop of a row
 /*****************************************************/
-function InitializeRow(){
+function onDropRow(){
 	var aione_rows = new Array();
 	$(".aione-builder .ar").each(function() {
 		var aione_row_id = $(this).attr("id");
@@ -159,11 +186,89 @@ function InitializeRow(){
 		});
     });
 }
+/*****************************************************
+/*   function triggers on drop of a column
+/*****************************************************/
+function onDropColumn(){
+	var aione_column = new Array();
+	$(".aione-builder .ac").each(function() {
+		var aione_column_id = $(this).attr("id");
+		if(aione_column_id != undefined){
+			aione_column.push(aione_column_id);
+		} else {
+			var aione_column_id = 'col_id_'+Math.floor(Math.random()*100000000);
+			$(this).attr("id", aione_column_id);
+			var columnHeader = $('.aione-options').html();
+			$(this).html(columnHeader);
+			aione_column.push(aione_column_id);
+		}
+        
+    });
+
+    //columns 
+    $.each(aione_column, function( index, aione_colm ) {
+    	var aione_column_id = document.getElementById(aione_colm);
+    	
+    	Sortable.create(aione_column_id,{
+			//group: "name_"+(index+1),  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
+			group: { name: "columns", pull: true, put: ['aione_elements'] },
+			sort: true,  // sorting inside list
+			delay: 0, // time in milliseconds to define when the sorting should start
+			disabled: false, // Disables the sortable if set to true.
+			store: null,  // @see Store
+			handle: '.aione-col-handle',
+			animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
+		});
+    });
+}
+
+
+/*****************************************************
+/*  Initialize aione builder elements on sidebar
+/*****************************************************/
+function initBuilderElements() {
+	var aione_elements_section = document.getElementById('aione_elements_section');
+	Sortable.create(aione_elements_section,{
+		group: { name: "aione_elements_section", pull: 'clone', put: true },
+		sort: false,
+		onEnd: function(){
+				setTimeout(function(){
+					onDropSection();
+				},200);
+			}
+	});
+
+	var aione_elements_row = document.getElementById('aione_elements_row');
+    Sortable.create(aione_elements_row,{
+		group: { name: "aione_elements_row", pull: 'clone', put: ['aione_elements_columns'] },
+		sort: false,
+		onEnd: function(){
+					setTimeout(function(){
+						onDropRow();
+						
+					},200);
+					
+				}
+    });
+
+    var aione_elements_columns = document.getElementById('aione_elements_columns');
+    Sortable.create(aione_elements_columns,{
+		group: { name: "aione_elements_columns", pull: 'clone', put: ['aione_elements'] },
+		sort: false,
+		onEnd: function(){
+					setTimeout(function(){
+						onDropColumn();
+						
+					},200);
+					
+				}
+    });
+}
 $(document).ready(function() {
 	init_columns();
 	init_rows();
 	init_sections();
-	
+	initBuilderElements();
 
 
 
@@ -181,49 +286,48 @@ $(document).ready(function() {
 		$(".aione-builder-wrapper").toggleClass("mode-preview");
 		$(".aione-builder-wrapper").toggleClass("mode-editable");
 	});
+		$('body').on('click','.clone',function(){
+			var insertAfter = $(this).parents('.ac').attr('id');
+			$('#'+insertAfter).parent().append($('#'+insertAfter).clone()).html();
+			
+			
+		});	
 
-	var aione_elements_section = document.getElementById('aione_elements_section');
-	Sortable.create(aione_elements_section,{
-		group: { name: "aione_elements_section", pull: 'clone', put: true },
-		sort: false,
-		onEnd: function(){
-				setTimeout(function(){
-					initializaSection();
-				},200);
+		//decressWidth of column
+		$('body').on('click','.decress-width',function(){
+			var id = $(this).parents('.ac').attr('id');
+			if(/l([1-9][0-9]?|100)\b/.test($(this).parents('.ac').attr('class'))){
+
+				var classLength = $(this).parents('.ac').attr('class').match(/l([1-9][0-9]?|100)\b/)[1];
+				var newclass = $(this).parents('.ac').attr('class').replace(/l([1-9][0-9]?|100)\b/,'l'+(parseInt(classLength)-4));
+				$('#'+id).attr('class',newclass);
 			}
-	});
-	initializaSection();
+		});
 
-	var aione_builder = document.getElementById('aione_section');
-	Sortable.create(aione_builder,{
-		//group: "name_"+(index+1),  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
-		group: { name: "aione_builder_sections", pull: false, put: ['aione_elements_row'] },
-		sort: true,  // sorting inside list
-		delay: 0, // time in milliseconds to define when the sorting should start
-		disabled: false, // Disables the sortable if set to true.
-		store: null,  // @see Store
-		animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
-		// handle: ".aione-section-handle",
-		ghostClass: "ghost-section", 
-		// chosenClass: "chosen-section",
-		dragClass:"drag-section",
-		//filter: '.ar , .ac'
-	});
+		//incressWidth of column
+		$('body').on('click','.incress-width',function(){
+			var id = $(this).parents('.ac').attr('id');
+			if(/l([1-9][0-9]?|100)\b/.test($(this).parents('.ac').attr('class'))){
+
+				var classLength = $(this).parents('.ac').attr('class').match(/l([1-9][0-9]?|100)\b/)[1];
+				var newclass = $(this).parents('.ac').attr('class').replace(/l([1-9][0-9]?|100)\b/,'l'+(parseInt(classLength)+4));
+				$('#'+id).attr('class',newclass);
+			}
+		});
+
+		$(document).on('click','.delete-column',function(){
+			$(this).parents('.aione-option-bar').parent().remove();
+		});
+
+
+
+		//right side accordian
+		$('body').on('click','.aione_collapse_element',function(){
+			$(this).siblings('div').slideToggle();
+			$(this).find('i').toggleClass('fa-minus');
+		});
+
 	
-
-	var aione_elements_row = document.getElementById('aione_elements_row');
-	Sortable.create(aione_elements_row,{
-		group: { name: "aione_elements_row", pull: 'clone', put: ['aione_elements_columns'] },
-		sort: false,
-		onEnd: function(){
-					setTimeout(function(){
-						InitializeRow();
-						
-					},200);
-					
-				}
-	});
-	InitializeRow();
 
 });
 
